694cd61950ceba5fdb5d127bc6a3898d43fd7022
Weedzard <arndslol@gmail.com>
2022-11-16 18:42:57 +0100

    Shooting system

--- Assets/Scripts/AimControls.cs
+++ Assets/Scripts/AimControls.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class AimControls : MonoBehaviour
 {
     public GameObject tank;
+    public GameObject barrelEnd;
+    public GameObject bulletPrefab;
+    private float nextShot = 0.15f;
+    private float cooldown=0.5f;
 
     // Update is called once per frame
     void Update()
@@ -15,5 +19,17 @@ public class AimControls : MonoBehaviour
         }
         else if (Input.GetKey(KeyCode.D))
             transform.RotateAround(tank.transform.position, -Vector3.forward, 40f * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.Space) && Time.time>nextShot)
+        {
+            ProjectileShoot();
+        }
+    }
+
+    private void ProjectileShoot()
+    {
+        GameObject projectileGO = (GameObject)Instantiate(bulletPrefab, barrelEnd.transform.position,
+            transform.rotation);
+        nextShot = Time.time + cooldown;
     }
 }
---
+++ Assets/Scripts/BulletLogic.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletLogic : MonoBehaviour
+{
+    private float speed = 10f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position += transform.right * Time.deltaTime * speed;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
