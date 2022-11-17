6a5d4409608a14a618268498c63e4f95b0115023
WittIsHere <59049883+WittIsHere@users.noreply.github.com>
2022-11-17 15:11:03 +0100

    Sending TestClass (minitank)
    ABRAHAM SOLO DIJO IP RARA, pero sirvió
    
    Co-Authored-By: Pau Fiol <45202003+paufiol@users.noreply.github.com>

--- Assets/Scripts/JoinServer.cs
+++ Assets/Scripts/JoinServer.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
+using System.IO;
 using UnityEngine.UI;
 
 public class JoinServer : MonoBehaviour
@@ -30,14 +31,26 @@ public class JoinServer : MonoBehaviour
 
     [HideInInspector] public bool messageSent = false;
 
+    MemoryStream mem = new MemoryStream();
 
     private bool joined = false;
 
     [HideInInspector] public Thread sendthread;
 
+    class testClass
+    {
+        public int hp = 10;
+        public Vector2 pos = new Vector2(1, 2);
+    }
+
+    testClass tank1 = new testClass();
     void Start()
     {
         textCanvas.GetComponent<Canvas>().enabled = false;
+
+        tank1.hp = 8;
+        tank1.pos.x = 0;
+        tank1.pos.y = 0;
     }
 
     public void Join()
@@ -52,7 +65,7 @@ public class JoinServer : MonoBehaviour
         // Server Endpoint
         ipepServer = new IPEndPoint(IPAddress.Parse(ipString), port);
 
-        data = new byte[256];
+        data = new byte[1024];
         data = Encoding.ASCII.GetBytes(usernameString);
 
         messageSent = true;
@@ -83,20 +96,37 @@ public class JoinServer : MonoBehaviour
     {
         while (true)
         {
-            if (messageSent)
-            {
-                newSocket.SendTo(data, data.Length, SocketFlags.None, ipepServer);
-                messageSent = false;
-            }
+            //if (messageSent)
+            //{
+            //    newSocket.SendTo(data, data.Length, SocketFlags.None, ipepServer);
+            //    messageSent = false;
+            //}
 
-            newSocket.ReceiveFrom(data, ref ipepServer);
-            recData = Encoding.ASCII.GetString(data);
+            //newSocket.ReceiveFrom(data, ref ipepServer);
+            //recData = Encoding.ASCII.GetString(data);
 
-            if (recData != null)
-            {
-                recTrue = true;
-            }
+            //if (recData != null)
+            //{
+            //    recTrue = true;
+            //}
+
+            SerializeJson(tank1);
+            Debug.Log(mem.GetBuffer().Length.ToString());
+            newSocket.SendTo(mem.GetBuffer(), mem.GetBuffer().Length, SocketFlags.None, ipepServer);
+            Debug.Log("Message sent: " + tank1.hp.ToString() + " " + tank1.pos.y.ToString() + " " + tank1.pos.x.ToString());
+
+  
 
         }
+    }
+
+    void SerializeJson(testClass a)
+    {
+
+        mem = new MemoryStream();
+        string json = JsonUtility.ToJson(a);
+        BinaryWriter writer = new BinaryWriter(mem);
+        writer.Write(json);
+        Debug.Log("Serialized");
     }
 }
