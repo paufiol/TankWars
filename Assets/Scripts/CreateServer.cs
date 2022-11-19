e6cebf46f9dd0160f5a93e63b675233cf8baa728
WittIsHere <59049883+WittIsHere@users.noreply.github.com>
2022-11-19 18:10:56 +0100

    Revert "who f knows what this will do"
    This reverts commit 312ea1632ac9ce09a8364bd8ba4b0e5ed0f39bb9.

--- Assets/Scripts/CreateServer.cs
+++ Assets/Scripts/CreateServer.cs
@@ -12,18 +12,16 @@ using System.Linq;
 
 public class CreateServer : MonoBehaviour
 {
-    //Chat and messages
     public Text message;
     public Canvas canvas;
     public Canvas textCanvas;
 
-    // Socket
     [HideInInspector] public Socket newSocket;
     private int port = 5631;
 
     [HideInInspector] public string hostIP;
 
-    // More sockets stuff
+    // Sockets stuff
 
     [HideInInspector] public string recData;
     [HideInInspector] public bool recTrue = false;
@@ -31,13 +29,8 @@ public class CreateServer : MonoBehaviour
 
     [HideInInspector] public EndPoint ipepClient;
 
-    // Declare Threads
     [HideInInspector] public Thread recthread;
-    [HideInInspector] public Thread sendthread;
 
-    // vars for serialization
-
-    MemoryStream mem = new MemoryStream();
     private byte[] data;
     private string json;
 
@@ -49,17 +42,12 @@ public class CreateServer : MonoBehaviour
         public Quaternion rot;
     }
 
-    // The class that will store the values of the client tank
-    private tankClass enemyTankClass;
-    private tankClass myTankClass;
+    private tankClass enemyTank;
 
     //Tank and spawn
     public GameObject tankPrefab;
     public GameObject spawn;
-    public GameObject enemySpawn; //client tank spawn
-
-    [HideInInspector] public GameObject hostTank;
-    [HideInInspector] public GameObject clientTankCopy;
+    public GameObject enemySpawn;
 
     // Create a list where we will store the tanks
     private List<GameObject> tankInstances = new List<GameObject>();
@@ -72,8 +60,7 @@ public class CreateServer : MonoBehaviour
     public void Create()
     {
         data = new byte[256];
-        enemyTankClass = new tankClass();
-        myTankClass = new tankClass();
+        enemyTank = new tankClass();
 
         // Creating UDP Socket
         newSocket = new Socket(AddressFamily.InterNetwork,
@@ -87,22 +74,17 @@ public class CreateServer : MonoBehaviour
 
         recthread = new Thread(Rec);
         recthread.Start();
-
-        sendthread = new Thread(Send);
-        sendthread.Start();
-
         message.text = " Server created with IP: " + GetLocalIPv4();
         canvas.GetComponent<Canvas>().enabled = false;
         textCanvas.GetComponent<Canvas>().enabled = true;
 
 
-        // Create the tank that will be controled by the host (us)
-        hostTank = Instantiate(tankPrefab, spawn.transform.position,
+        // Create the tank that will be controled by the host
+        GameObject hostTank = Instantiate(tankPrefab, spawn.transform.position,
             transform.rotation);
 
         tankInstances.Add(hostTank);
 
-<<<<<<< Updated upstream
         //hostTankControls.isPlayer = false;
 
         //TankControls isPlayer=hostTank.GetComponent<TankControls>();
@@ -110,77 +92,32 @@ public class CreateServer : MonoBehaviour
 
         //Debug.Log(tankInstances.Count);
 
-=======
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
         // Create the tank that will be controled by the client
         GameObject clientTank = (GameObject)Instantiate(tankPrefab, enemySpawn.transform.position, transform.rotation);
         SpriteRenderer sprite = clientTank.GetComponent<SpriteRenderer>();
-=======
-        //hostTankControls.isPlayer = false;
-
-        //TankControls isPlayer=hostTank.GetComponent<TankControls>();
-        //isPlayer.DisableTank();
-
-        //Debug.Log(tankInstances.Count);
-
-        // Create the tank that will be controled by the client (not us)
-        clientTankCopy = (GameObject)Instantiate(tankPrefab, enemySpawn.transform.position, transform.rotation);
-        SpriteRenderer sprite = clientTankCopy.GetComponent<SpriteRenderer>();
->>>>>>> Stashed changes
         sprite.color = Color.red;
 
-        tankInstances.Add(clientTankCopy);
+        tankInstances.Add(clientTank);
     }
 
     void Update()
     {
-<<<<<<< Updated upstream
         //Disable 2nd tank controls
         if(tankInstances.Count>0)
-=======
-<<<<<<< Updated upstream
-        //Make sure there is at least one tank
-=======
-        // we update the class with our information
-        if (tankInstances.Count > 0)
-        {
-            myTankClass.pos.x = tankInstances[0].transform.position.x;
-            myTankClass.pos.y = tankInstances[0].transform.position.y;
-            myTankClass.rot = tankInstances[0].GetComponentInChildren<Transform>().Find("Cannon").rotation;
-            myTankClass.hp = tankInstances[0].GetComponent<TankControls>().GetHP();
-        }
-
-        //Disable 2nd tank controls
->>>>>>> Stashed changes
-        if (tankInstances.Count>0)
->>>>>>> Stashed changes
         {
             tankInstances[1].GetComponent<TankControls>().isEnabled = false;
         }
 
-        // if json is not null, we update the position of the client tank copy
         if (json != null)
         {
-            enemyTankClass = JsonUtility.FromJson<tankClass>(json);
+            enemyTank = JsonUtility.FromJson<tankClass>(json);
             
-            Debug.Log(enemyTankClass.pos.ToString());
-            Vector3 newPos = new Vector3(enemyTankClass.pos.x, enemyTankClass.pos.y, enemyTankClass.pos.z);
+            Debug.Log(enemyTank.pos.ToString());
+            Vector3 newPos = new Vector3(enemyTank.pos.x, enemyTank.pos.y, enemyTank.pos.z);
             tankInstances[1].transform.position = newPos;
         }
     }
 
-    void Send()
-    {
-        while (true)
-        {
-            // Serialize and send the data inside tankClass
-            SerializeJson(myTankClass);
-            Debug.Log(mem.GetBuffer().Length.ToString());
-            newSocket.SendTo(mem.GetBuffer(), mem.GetBuffer().Length, SocketFlags.None, ipepClient);
-            Debug.Log("Message sent: " + myTankClass.hp.ToString() + "POS " + myTankClass.pos.x.ToString() + " " + myTankClass.pos.y.ToString() + "Turret Rot:" + myTankClass.rot.ToString());
-        }
-    }
     void Rec()
     {
         while (true)
@@ -194,7 +131,6 @@ public class CreateServer : MonoBehaviour
 
             Debug.Log(json);
 
-           
             if (recData != null)
             {
                 recTrue = true;
@@ -213,14 +149,5 @@ public class CreateServer : MonoBehaviour
     private void OnApplicationQuit()
     {
         newSocket.Close();
-    }
-
-    void SerializeJson(tankClass a)
-    {
-        mem = new MemoryStream();
-        string json = JsonUtility.ToJson(a);
-        BinaryWriter writer = new BinaryWriter(mem);
-        writer.Write(json);
-        Debug.Log("Serialized");
     }
 }
--- Assets/Scripts/JoinServer.cs
+++ Assets/Scripts/JoinServer.cs
@@ -30,47 +30,26 @@ public class JoinServer : MonoBehaviour
 
     [HideInInspector] public bool messageSent = false;
 
+    MemoryStream mem = new MemoryStream();
+
     private bool joined = false;
 
-    // Declare Threads
     [HideInInspector] public Thread sendthread;
-    [HideInInspector] public Thread recthread;
 
     //Tank and spawn
     public GameObject tankPrefab;
     public GameObject spawn;
-<<<<<<< Updated upstream
 
     [HideInInspector] public GameObject joinTank;
-=======
-<<<<<<< Updated upstream
-    public GameObject enemySpawn;
-=======
-    public GameObject enemySpawn; //host tank spawn
-
-    [HideInInspector] public GameObject joinTank;
-    [HideInInspector] public GameObject hostTankCopy;
-
-    // vars for serialization
-
-    MemoryStream mem = new MemoryStream();
-    private byte[] data;
-    private string json;
->>>>>>> Stashed changes
->>>>>>> Stashed changes
 
     class tankClass
     {
         public float hp;
-        public Vector3 pos;
+        public Vector2 pos;
         public Quaternion rot;
     }
 
-    private tankClass myTankClass;
-    private tankClass hostTankClass;
-
-    // Create a list where we will store the tanks
-    private List<GameObject> tankInstances = new List<GameObject>();
+    private tankClass myTankClass = new tankClass();
 
     void Start()
     {
@@ -80,10 +59,6 @@ public class JoinServer : MonoBehaviour
 
     public void Join()
     {
-        data = new byte[256];
-        hostTankClass = new tankClass();
-        myTankClass = new tankClass();
-
         string ipString = ip.text;
         string usernameString = "User has Joined: " + username.text;
 
@@ -94,104 +69,33 @@ public class JoinServer : MonoBehaviour
         // Server Endpoint
         ipepServer = new IPEndPoint(IPAddress.Parse(ipString), port);
 
+        //data = new byte[1024];
+        //data = Encoding.ASCII.GetBytes(usernameString);
+
         messageSent = true;
 
-        // start the threads
         sendthread = new Thread(Send);
+
         sendthread.Start();
-
-        recthread = new Thread(Rec);
-        recthread.Start();
-
         message.text = "Server joined";
         canvasJoin.GetComponent<Canvas>().enabled = false;
         textCanvas.GetComponent<Canvas>().enabled = true;
 
-<<<<<<< Updated upstream
         // We instantiate the tank
         joinTank = (GameObject)Instantiate(tankPrefab, spawn.transform.position,
-=======
-<<<<<<< Updated upstream
-        // Create the tank that will be controled by the client
-        GameObject joinTank = (GameObject)Instantiate(tankPrefab, spawn.transform.position,
->>>>>>> Stashed changes
             transform.rotation);
 
         SpriteRenderer sprite = joinTank.GetComponent<SpriteRenderer>();
         sprite.color = Color.red;
-<<<<<<< Updated upstream
-=======
-        tankInstances.Add(joinTank);
-
-        // Create the tank that will be controled by the host
-        GameObject hostTank = Instantiate(tankPrefab, enemySpawn.transform.position,
-            transform.rotation);
-
-        tankInstances.Add(hostTank);
-=======
-        // We instantiate the tank (our tank)
-        joinTank = (GameObject)Instantiate(tankPrefab, spawn.transform.position,
-            transform.rotation);
-
-        tankInstances.Add(joinTank);
-
-        SpriteRenderer sprite = joinTank.GetComponent<SpriteRenderer>();
-        sprite.color = Color.red;
-
-        // We instantiate the tank for the host
-        hostTankCopy  = (GameObject)Instantiate(tankPrefab, enemySpawn.transform.position,
-            transform.rotation);
-
-        tankInstances.Add(joinTank);
->>>>>>> Stashed changes
->>>>>>> Stashed changes
     }
 
     void Update()
     {
-<<<<<<< Updated upstream
         // Each frame we actualize the content of tankClass
         myTankClass.pos.x = joinTank.transform.position.x;
         myTankClass.pos.y = joinTank.transform.position.y;
         myTankClass.rot = joinTank.GetComponentInChildren<Transform>().Find("Cannon").rotation;
         myTankClass.hp = joinTank.GetComponent<TankControls>().GetHP();
-=======
-<<<<<<< Updated upstream
-        //Make sure there is at least one tank
-        if (tankInstances.Count > 0)
-        {
-            //Disable 2nd tank controls
-            tankInstances[1].GetComponent<TankControls>().isEnabled = false;
-
-            // Each frame we update the content of tankClass
-            myTankClass.pos.x = tankInstances[0].transform.position.x;
-            myTankClass.pos.y = tankInstances[0].transform.position.y;
-            myTankClass.rot = tankInstances[0].GetComponentInChildren<Transform>().Find("Cannon").rotation;
-            myTankClass.hp = tankInstances[0].GetComponent<TankControls>().GetHP();
-        }
-
-        
-=======
-        if (tankInstances.Count > 0)
-        {
-            // Each frame we actualize the content of tankClass
-            myTankClass.pos.x = joinTank.transform.position.x;
-            myTankClass.pos.y = joinTank.transform.position.y;
-            myTankClass.rot = joinTank.GetComponentInChildren<Transform>().Find("Cannon").rotation;
-            myTankClass.hp = joinTank.GetComponent<TankControls>().GetHP();
-        }
-
-        // if json is not null, we update the position of the host tank copy
-        if (json != null)
-        {
-            hostTankClass = JsonUtility.FromJson<tankClass>(json);
-
-            Debug.Log(hostTankClass.pos.ToString());
-            Vector3 newPos = new Vector3(hostTankClass.pos.x, hostTankClass.pos.y, hostTankClass.pos.z);
-            tankInstances[1].transform.position = newPos;
-        }
->>>>>>> Stashed changes
->>>>>>> Stashed changes
     }
 
     void Send()
@@ -205,31 +109,6 @@ public class JoinServer : MonoBehaviour
             Debug.Log("Message sent: " + myTankClass.hp.ToString() + "POS " + myTankClass.pos.x.ToString() + " " + myTankClass.pos.y.ToString() + "Turret Rot:" + myTankClass.rot.ToString());
 
         }
-    }
-
-    void Rec()
-    {
-        while (true)
-        {
-            // We recieve data, then deserialize it
-            newSocket.ReceiveFrom(data, ref ipepServer);
-            MemoryStream stream = new MemoryStream(data);
-            BinaryReader reader = new BinaryReader(stream);
-            stream.Seek(0, SeekOrigin.Begin);
-            json = reader.ReadString();
-
-            Debug.Log(json);
-
-            if (recData != null)
-            {
-                recTrue = true;
-            }
-        }
-    }
-
-    private void OnApplicationQuit()
-    {
-        newSocket.Close();
     }
 
     void SerializeJson(tankClass a)
